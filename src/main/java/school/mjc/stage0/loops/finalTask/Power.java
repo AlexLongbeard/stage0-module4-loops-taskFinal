f4d0ae8ad804a10c840e78f5498376828c0ce8f3
AlexLongbeard <AGLBRDK.KR@gmail.com>
2023-02-25 15:38:13 +0600

    1

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,10 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
-    }
-
-    public static void main(String[] args) {
-        new Power().printPower(10, 3);
+        int result = 1;
+        if (power < 0) {
+            System.out.println("power is negative");
+        }
+        else if (power == 0) {
+            System.out.println("1");
+        }
+        else {
+            for (int i = 1; i <= power; i++) {
+                result = result * numberToPrint;
+            }
+            System.out.println(result);
+        }
     }
 }
