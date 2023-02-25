3ec0fc94be59f32b705b8ffe3becda7594d3cbf2
AlexLongbeard <AGLBRDK.KR@gmail.com>
2023-02-25 21:24:31 +0600

    2

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,10 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        if (cathetusLength==0) {
+            System.out.print(" ");
+        }
+        else {
+for (int i=1; i<=cathetusLength; i++) {
+    for (int j=1; j<=(cathetusLength-i); j++) {
+        System.out.print(" ");
     }
-
-    public static void main(String[] args) {
-        new HalfPyramid().printHalfPyramid(8);
+    for (int t=0; t<=(i-1); t++) {
+        System.out.print("*");
+    }
+    System.out.println("*");
+}
+    }
     }
 }
